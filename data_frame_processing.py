7e9abe151b18d0c68ffdf3ba9b832497c4442b90
Matias San Martin Acosta <msamartinacosta@gmail.com>
2016-09-09 20:04:35 -0300

    Se agrega pre-procesmiento de un texto para: - dejar caracteres permitidos - elminar stopwords - tokenizar
    Los stopwords se tomaron de http://www.ranks.nl/stopwords.
    
    Podría mejorarse usando los stopwords de nltk, pero habria que preguntar si nos permiten usarlo. De momento estan en un archivo y se levanta
    
    Se agrega un archivo de train con 10 lineas

--- data_frame_processing.py
+++ data_frame_processing.py
@@ -1,10 +1,28 @@
+import string
+
 def procesar_data_frame_train(dft):
-    return "";
+    return ""
 
 
 def procesar_data_frame_test(dft):
-    return "";
+    return ""
 
 
 def procesar_frame(frame):
-    return "";
+    return ""
+
+
+def pre_procesar_frame(frame):
+    # Cargo las stopwords
+    with open('english_stopwords.txt', 'rb') as f:
+        stopwords = [line.rstrip() for line in f]
+    stopwords_set = set(stopwords)
+
+    # Defino el set de caracteres permitidos en las palabras
+    whitelist = string.letters + string.digits + ' ' + '\''
+
+    # Filtro por el whitelist y las stopwords
+    filtered_string = ''.join(c for c in frame['Text'].lower() if c in whitelist).split()
+    result_with_no_stopwords = [i for i in filtered_string if i not in stopwords_set]
+
+    return result_with_no_stopwords
