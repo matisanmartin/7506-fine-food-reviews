131b9ed056e28132fd69a49bb8956c49711ab20e
cyng <cyng@cyn.cyn-dominio>
2016-11-20 22:23:47 -0300

    Arreglando archivo con los nuevos cambios

--- randomForest.py
+++ randomForest.py
@@ -1,30 +1,31 @@
 from sklearn.ensemble import RandomForestClassifier
-
 import read_and_process
-
 test_Id = []
-
-train = read_and_process.leer_archivo('train.csv', 'train')
-test = read_and_process.leer_archivo('test.csv', 'test')
-
 COLUMNAS = 9
 FILAS = 9
 matrix = []
 matrix1 = []
 vector_Prediction = []
 
+###### Carga de datos de los archivos csv-----------------------------------------------
+train = read_and_process.leer_archivo('train.csv', 'train')
+test = read_and_process.leer_archivo('test.csv', 'test')
+
+###### Preparando los datos para tenerlo en el formato correcto-------------------------
 for i in range(FILAS):
 	matrix.append([])
         for j in range(COLUMNAS):
             matrix[i].append(0)
 
 for indice in range(FILAS):
-        vector_Prediction.append((float(train[indice]['Prediction'])))
-        texto = train[indice]['Text'].split()
+        vector_Prediction.append((float(train['Prediction'][indice])))
+        texto = train['Text'][indice].split()
 for j in range(len(texto)):
         hash_val = hash(texto[j]) % COLUMNAS
         matrix[indice][hash_val] += 1
 
+
+###### Utilizamos la implementacion para entrenar el algoritmo----------------------
 rf = RandomForestClassifier(bootstrap=True, criterion='gini',
             max_depth=None, max_features='auto',
             min_samples_leaf=1, min_samples_split=2,
@@ -32,7 +33,7 @@ rf = RandomForestClassifier(bootstrap=True, criterion='gini',
             oob_score=False, random_state=None, verbose=0)
 rf.fit(matrix, vector_Prediction)
 
-###### Test
+###### Test--------------------------------------------------------------------------
 
 test_Id = []
 salida_predicciones = []
@@ -45,13 +46,16 @@ for i in range(FILAS2):
             matrix1[i].append(0)
 
 for indice in range(FILAS2):
-        test_Id.append(test[indice]['Id'])
-        texto = test[indice]['Text'].split()
+        test_Id.append(test['Id'][indice])
+        texto = test['Text'][indice].split()
         for j in range(len(texto)):
             hash_val = hash(texto[j]) % COLUMNAS2
             matrix1[indice][hash_val] += 1
-        print(texto)
+
+###### Predecir :)------------------------------------------------------------------------
 mat_aux = rf.predict(matrix1)
+
+###### Generar salida para submittear-----------------------------------------------------
 for i in range(FILAS2):
         prediccion = mat_aux[i]
         salida_predicciones.append({'Id': test_Id[i], 'Prediction': prediccion})
